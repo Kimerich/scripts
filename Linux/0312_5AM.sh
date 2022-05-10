be91f6c66578049767564b9fc18e9ab882e2aaae
Eric Kim <98678493+Kimerich@users.noreply.github.com>
2022-05-09 23:33:50 -0400

    Delete 0312_5AM.sh

--- Linux/0312_5AM.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-awk '{print $1,$2,$5,$6}' 0312_Dealer_schedule | grep "05:00:00 AM" # find the roulette dealer at the specified time
-awk '{print $1,$2,$5,$6}' 0312_Dealer_schedule | grep "05:00:00 AM" >> Dealers_working_during_loss # append to listed file
