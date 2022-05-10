c95bf7375eeef4d414ef1b0b92c199240d205e27
Eric Kim <98678493+Kimerich@users.noreply.github.com>
2022-05-09 23:33:19 -0400

    Delete roulette_dealer_finder_by_time.sh

--- Linux/roulette_dealer_finder_by_time.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script should take 3 arguments (time, date, game to be played) and find who the roulette dealer was at the time.
-# Format should be hh:mm:ss ?M MMDD game(will not be used as the title program name implies you just need the roulette dealer)
-# This program also assumes all the schedule files are in the same directory as the program
-
-awk '{print $1,$2,$5,$6}' $1_Dealer_schedule | grep $2 | grep $3
-
-
