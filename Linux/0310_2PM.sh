103b1def07a338899e7c84354dd16a1f7ce53c8c
Eric Kim <98678493+Kimerich@users.noreply.github.com>
2022-05-09 23:32:58 -0400

    Delete 0310_2PM.sh

--- Linux/0310_2PM.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-awk '{print $1,$2,$5,$6}' 0310_Dealer_schedule | grep "02:00:00 PM" # find the roulette dealer at the specified time
-awk '{print $1,$2,$5,$6}' 0310_Dealer_schedule | grep "02:00:00 PM" >> Dealers_working_during_loss # append to listed file
